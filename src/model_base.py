f081d6315a4185229f0c45ed06e96e013ac12c75
NurullahYusufDuru <yusuf.duru@ozu.edu.tr>
2026-03-06 15:32:00 +0300

    Add base allocation model implementation
    This script implements the base allocation model using a piecewise linear MILP formulation.

---
+++ src/model_base.py
@@ -0,0 +1,10 @@
+"""
+Replication of the allocation model from
+
+Azizi et al. (2021)
+Aid Allocation for Camp-Based and Urban Refugees with
+Uncertain Demand and Replenishments.
+
+This script will implement the base allocation model
+using a piecewise linear MILP formulation.
+"""
