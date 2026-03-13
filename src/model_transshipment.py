801642d92c090696480391d898102d136cc35dd2
NurullahYusufDuru <yusuf.duru@ozu.edu.tr>
2026-03-13 15:05:18 +0300

    Refactor model to use build_model and optimize
    Refactored the model to import and use the build_model function from model_v1. Added optimization and output for the optimal solution.

--- src/model_transshipment.py
+++
@@ -1,9 +0,0 @@
-"""
-Extension of the original model.
-
-This module will introduce lateral transshipment
-between refugee camps.
-
-Decision variable:
-T_ij : inventory transferred from camp i to camp j
-"""
